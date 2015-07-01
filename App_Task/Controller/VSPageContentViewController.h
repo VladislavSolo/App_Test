1e2a6af016000dab5492ade2f5f06f262ca45814
Vladislav Stanisheskij <Vladislav-work1909@yandex.by>
2015-07-01 21:27:41 +0300

    add PageViewController

---
+++ App_Task/Controller/VSPageContentViewController.h
@@ -0,0 +1,15 @@
+//
+//  VSPageViewController.h
+//  App_Task
+//
+//  Created by Владислав Станишевский on 7/1/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface VSPageContentViewController : UIViewController 
+
+@property (assign, nonatomic) NSUInteger pageIndex;
+
+@end
---
+++ App_Task/Controller/VSPageContentViewController.m
@@ -0,0 +1,25 @@
+//
+//  VSPageViewController.m
+//  App_Task
+//
+//  Created by Владислав Станишевский on 7/1/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import "VSPageContentViewController.h"
+
+@implementation VSPageContentViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+        
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
