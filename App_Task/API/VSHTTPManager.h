b45504600c360ec1325741a3897df44abfe2c120
Vladislav Stanisheskij <Vladislav-work1909@yandex.by>
2015-07-02 01:03:55 +0300

    add VSWidgetViewController to PageViewController

--- App_Task/API/VSHTTPManager.h
+++ App_Task/API/VSHTTPManager.h
@@ -1,17 +1,13 @@
 //
-//  VSHTTPManager.h
+//  VSHTTPManage.h
 //  App_Task
 //
-//  Created by Владислав Станишевский on 7/1/15.
+//  Created by Владислав Станишевский on 7/2/15.
 //  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@class VSCitation;
-
-@interface VSHTTPManager : NSObject
-
-- (void)getRandomCitationOnSuccess:(void(^)(VSCitation* citation))succeess onFailure:(void(^)(NSError* error))failure;
+@interface VSHTTPManage : NSObject
 
 @end
