5305a38ececde35ea1c20faece0f6450cf2a35c1
Vladislav Stanisheskij <Vladislav-work1909@yandex.by>
2015-07-02 00:52:36 +0300

    add VSWidgetViewController to PageViewController

---
+++ App_Task/Controller/VSWidgetViewController.h
@@ -0,0 +1,13 @@
+//
+//  VSWidgetViewController.h
+//  App_Task
+//
+//  Created by Владислав Станишевский on 7/1/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSWidgetViewController : UIViewController
+
+@end
---
+++ VIew/VSCitationView.m
@@ -0,0 +1,13 @@
+//
+//  VSCitationView.m
+//  App_Task
+//
+//  Created by Владислав Станишевский on 7/1/15.
+//  Copyright (c) 2015 Vlad Stanishevskij. All rights reserved.
+//
+
+#import "VSCitationView.h"
+
+@implementation VSCitationView
+
+@end
